bc54946e9a470eae3ef5cf5fafa92ac9a4120b0a
Kwentine <quentin.agren@gmail.com>
2017-10-01 14:19:08 +0200

    HTTP fetch w/ coroutines

---
+++ eventloop.py
@@ -0,0 +1,113 @@
+from selectors import DefaultSelector
+
+# Possible states a future can be in
+_PENDING = 'PENDING'
+_FINISHED = 'FINISHED' 
+_CANCELLED = 'CANCELLED' # Not used in this simplified implementation
+
+
+class InvalidStateError(Exception):
+    """Operation not allowed in this state"""
+
+
+class Future:
+    """Promise of a future value
+    """
+    
+    _STATE = _PENDING
+    _result = None
+
+    def __init__(self):
+        self._callbacks = []
+
+    def done(self):
+        return self._STATE != _PENDING
+
+    def result(self):
+        if self._STATE != _FINISHED:
+            raise InvalidStateError('Result is not ready')
+        return self._result
+    
+    def __iter__(self):
+        if not self.done():
+            yield self
+        return self.result()
+
+    def add_done_callback(self, fn):
+        """ Schedule a callback to be called when the future resolves.
+
+        If the future is resolved, call immediatly.
+        """
+        if self._STATE == _FINISHED:
+            fn(self)
+        else:
+            self._callbacks.append(fn)
+
+    def set_result(self, result):
+        """Mark Future done and set its result"""
+
+        # A result can only be set on a pending future
+        if self._STATE != _PENDING:
+            raise InvalidStateError('Setting result on non-pending Future')
+        self._result = result
+        self._STATE = _FINISHED
+
+        # Run callbacks
+        self._schedule_callbacks()
+        
+    def _schedule_callbacks(self):
+        """Run callbacks with self as single argument.
+
+        In asyncio, callbacks are not run immediatly but schedule to
+        be call soon in the event loop.
+
+        """
+        for callback in self._callbacks:
+            callback(self)
+        self._callbacks[:] = []
+        
+class Task(Future):
+    """Coroutine wrapped in a future
+    """
+    def __init__(self, coro):
+        super().__init__()
+        self.coro = coro
+            
+    def _step(self):
+        """Advance coroutine one step"""
+        try:
+            result = self.coro.send(None)
+            result.add_done_callback(self._wakeup)
+        except StopIteration as exc:
+            self.set_result(exc.value)
+
+    def _wakeup(self, future):
+        self._step()
+
+class EventLoop:
+
+    def __init__(self):
+        self.selector = DefaultSelector()
+
+    def run_until_complete(self, future):
+        """Run a Future until completion and return its result"""
+        pass
+
+    def run_forever(self):
+        while True:
+            events = self.selector.select()
+            for key, event in events:
+                callback = key.data
+                callback()
+            
+                
+def task(gen_func):
+    """Decorator function that wraps coroutine into a task"""
+    def wrapped(*args, **kwargs):
+        coro = gen_func(*args, **kwargs)
+        task = Task(coro)
+        task._step()
+        return task
+    return wrapped
+
+loop = EventLoop()
---
+++ fetchcoro.py
@@ -0,0 +1,48 @@
+from eventloop import Future, Task, task, loop
+from selectors import EVENT_READ, EVENT_WRITE
+import socket
+
+@task
+def fetch(host):
+    sock = yield from connect(host, 80)
+    request = 'GET / HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n'.format(host)
+    sock.send(request.encode('utf8'))
+    response = yield from read_all(sock)
+    return response
+
+def connect(host, port):
+    sock = socket.socket()
+    sock.setblocking(False)
+    try:
+        sock.connect((host, port))
+    except BlockingIOError:
+        pass
+    f = Future()
+    def on_connected():
+        f.set_result(True)
+    loop.selector.register(sock, EVENT_WRITE, data=on_connected)
+    yield from f
+    loop.selector.unregister(sock)
+    return sock
+
+def read(sock):
+    f = Future()
+    def on_readable():
+        f.set_result(sock.recv(4096))
+    loop.selector.register(sock, EVENT_READ, on_readable)
+    chunk = yield from f
+    loop.selector.unregister(sock)
+    return chunk
+
+def read_all(sock):
+    response = []
+    chunk = yield from read(sock)
+    while chunk:
+        response.append(chunk)
+        chunk = yield from read(sock)
+    return b''.join(response)    
+
+if __name__ == "__main__":
+    t = fetch('www.dabeaz.com')
+    t.add_done_callback(lambda f: print('Response size: {}'.format(len(f.result()))))
+    loop.run_forever()
