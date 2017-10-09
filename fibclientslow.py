334842412dd57aa5bafce46f4ccfd17022e963f4
Quentin Agren <quentin.agren@sciencespo.fr>
2017-10-09 10:44:28 +0200

    Pass argument size on command line

--- fibclientslow.py
+++ fibclientslow.py
@@ -1,12 +1,15 @@
+import sys
 import socket
 import time
 
 s = socket.socket()
 s.connect(('localhost', 5000))
 
+n = sys.argv[1] if len(sys.argv) > 1 else 30
+
 while True:
     start = time.time()
-    s.send(b'30')
+    s.send(str(n).encode('ascii'))
     s.recv(256)
     end = time.time()
     print(end - start)
