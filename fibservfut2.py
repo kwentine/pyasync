c8c8737a92a336c2a50ded3feb862df4e4be3239
Quentin Agren <quentin.agren@sciencespo.fr>
2017-10-09 11:43:47 +0200

    Add support for futures to selector-based fibserv

--- fibservfut2.py
+++ fibservfut2.py
@@ -1,9 +1,13 @@
 import socket
+import functools
 from selectors import DefaultSelector, EVENT_READ, EVENT_WRITE
+from concurrent.futures import ThreadPoolExecutor as TPool
+from concurrent.futures import ProcessPoolExecutor as PPool
 from collections import deque
 
-selector = DefaultSelector()
 
+selector = DefaultSelector()
+pool = TPool(3)
 tasks = deque()
 
 def loop():
@@ -18,10 +22,19 @@ def run_tasks():
     while tasks:
         try:
             task = tasks.popleft()
-            event, sock = next(task) # May raise StopIteration
-            selector.register(sock, event, data=task)
+            event, awaited = next(task) # May raise StopIteration
+            if event == 'future':
+                future_notify, future_event = socket.socketpair()
+                selector.register(future_event, EVENT_READ, data=task)
+                awaited.add_done_callback(functools.partial(on_future_done, future_notify))
+            else:
+                selector.register(awaited, event, data=task)
         except StopIteration:
             continue
+
+def on_future_done(sock, future):
+    sock.send(b'1')
+    sock.close()                       
         
 def fib(n):
     """Compute nth Fibonacci number"""
@@ -35,13 +48,16 @@ def handle_client(conn):
     while True:
         yield EVENT_READ, conn # Suspend before reading
         try:
-            data = conn.recv(256) 
+            data = conn.recv(256)
             n = int(data.decode('ascii'))
-            res = str(fib(n)) + '\n'
-        except:
+            future = pool.submit(fib, n)
+            yield 'future', future
+            res = str(future.result()) + '\n' # Result is available by now
+            conn.send(res.encode('ascii')) # Won't block unless network buffer is full            
+        except Exception as e:
+            print(e.__class__.__name__, e)
             conn.close()
             break
-        conn.send(res.encode('ascii')) # Won't block unless network buffer is full
 
 def run_server(host='127.0.0.1', port=5000):
     s = socket.socket()
