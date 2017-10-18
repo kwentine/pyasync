86e2192528b148b99e2e8c7c32ec073ae6cf9c6f
Quentin Agren <quentin.agren@sciencespo.fr>
2017-10-18 12:19:01 +0200

    Fibservcb tested

--- fibservcb.py
+++ fibservcb.py
@@ -1,49 +1,42 @@
 import socket
-from selectors import DefaultSelector, EVENT_READ, EVENT_WRITE
-
+from fib import fib
+from selectors import DefaultSelector, EVENT_READ
 selector = DefaultSelector()
-
-def fib(n):
-    """Compute nth Fibonacci number"""
-    if n <= 1:
-        return n
-    else:
-        return fib(n-1) + fib(n-2)
-
-def handle_client(conn):
-    # Socket ready for reading
-    try: 
-        data = conn.recv(256)
-        if not data:
-            raise Exception('Connection closed by {}:{}'.format(*conn.getpeername()))
-        n = int(data.decode('ascii'))
-        res = str(fib(n)).encode('ascii') + b'\n'
-        conn.send(res)
-    except Exception as e:
-        print(e)
-        selector.unregister(conn)
-        conn.close()
-    
-def accept(sock):
-    conn, addr = sock.accept()
-    print('Connection from {}:{}'.format(*addr))
-    selector.register(conn, EVENT_READ, handle_client)
 
 def start_server(host='127.0.0.1', port=5000):
     s = socket.socket()
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.setblocking(False) # Make socket non-blocking
     s.bind((host, port))
     s.listen(5)
     print("Fibonacci server running on port %d ..." % port)
+    # Register callback for when a connection arrives
     selector.register(s, EVENT_READ, data=accept)
+
+def accept(sock):
+    conn, addr = sock.accept()
+    # Register callback for when client data arrives
+    selector.register(conn, EVENT_READ, handle_client)
+
+def handle_client(conn):
+    try: 
+        data = conn.recv(256)
+        n = int(data.decode('ascii'))
+        result = fib(n)
+        response = str(result).encode('ascii') + b'\n'
+        conn.send(response)
+    except:
+        selector.unregister(conn)
+        conn.close()
 
 def loop():
     while True:
+        # Blocks until a registered socket is ready for I/O
         events = selector.select()
         for key, mask in events:
+            # key.fileobj : registered socket
+            # key.data    : associated callback
             callback = key.data
-            callback(key.fileobj)
+            callback(key.fileobj) 
     
 if __name__ == "__main__":
     start_server()
--- fibservthrd.py
+++ fibservthrd.py
@@ -1,27 +1,7 @@
-# Basic UDP server application
-from time import sleep
+from fib import fib
+from fibservsync import handle_client
 from threading import Thread
 import socket
-
-
-def fib(n):
-    """Compute nth Fibonacci number"""
-    if n <= 1:
-        return n
-    else:
-        return fib(n-1) + fib(n-2)
-
-def handle_client(conn):
-    """Receive integer from client and send back its square"""
-    with conn:
-        while True:
-            data = conn.recv(256) # Blocking
-            try:
-                n = int(data.decode('ascii'))
-            except ValueError:
-                break
-            res = str(fib(n)) + '\n'
-            conn.send(res.encode('ascii'))
 
 def run_server(host='127.0.0.1', port=5000):
     s = socket.socket()
